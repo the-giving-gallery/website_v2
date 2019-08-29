a36fd17218ee99ed957abacc308e3662117c1730
thedanielenrico <thedanielenrico@gmail.com>
2019-08-29 12:27:49 -0700

    start on sold art section

---
+++ client/src/ImageArray.js
@@ -0,0 +1,23 @@
+const imgArray = [
+    {
+        id: 1,
+        url: "/utils/images/IMG_9317.JPG",
+    },
+    {
+        id: 2,
+        url: "/utils/images/IMG_9723.JPG",
+    },
+    {
+        id: 3,
+        url: "/utils/images/IMG_4448.JPG",
+    },
+    {
+        id: 4,
+        url: "/utils/images/IMG_4450.JPG",
+    },
+    {
+        id: 5,
+        url: "/utils/images/IMG_4451.JPG",
+    },
+];
+export default imgArray
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import "./index.css";
-// import { BrowserRouter as Router, Route, NavLink, Switch } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-// import Gallery from "../Gallery/Gallery"
+import SoldArt from "../../ImageArray"
 
 
 
 function Home() {
   return (
     <>
-      <div class="hero">
-        <div class="hero-text">
+      <div className="hero">
+        <div className="hero-text">
           <h1>Getting Creative with Mental Health</h1>
           <NavLink to="/gallery">
             <button className="btn bg-white" >Browse Art</button>
@@ -19,10 +18,10 @@ function Home() {
       </div>
       <div id="callToAction" className="container">
         <div className="row text-center">
-          <h2 className="col-md-12">Buy art or sell art that supports mental health nonprofits.</h2>
+          <h2 className="col-md-12 mt-5">Buy or sell art that supports mental health nonprofits.</h2>
 
         </div>
-        <div className="row text-center m-4">
+        <div className="row text-center mt-5 mb-5">
 
           <NavLink to="/gallery" className="col-md-4 actionBtn">
             <button className="btn bg-white actionBtn" >Browse Art</button>
@@ -34,6 +33,15 @@ function Home() {
             <button className="btn bg-white actionBtn" >Partner</button>
           </NavLink>
         </div>
+        <hr ></hr>
+        <div className="row text-left m-5">
+          <p>The Giving Gallery is a free online platform for artists to sell their artwork and share their thoughts about mental health. Art sale proceeds are split between the artist and a partnered mental health nonprofit of the buyer's choice.</p>
+        </div>
+        <hr></hr>
+      </div>
+      <div className="container" id="soldArt">
+        <h1>sold art</h1>
+        {SoldArt.map(image => <img src={image.url} alt="sold artwork" className="soldArt" key={image.id}></img>)}
       </div>
 
 
